0a993950ff69ccfc249116aa86f906880fd2e6f3
Hecatomb <60758685+pedroelbanquero@users.noreply.github.com>
2021-09-12 09:34:38 -0500

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,5 +1,6 @@
 # compiler eos shell metamorph
 
+cython3 --embed -o shelleos.c shell-eos.py
 gcc -static shelleos.c -o shelleos -I /usr/include/python3.5 $(pkg-config --libs --cflags python3) -lpython2.7 -lm -lutil -ldl -lz -lexpat -lpthread -lc
 
 
