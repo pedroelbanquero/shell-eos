cc4b3eceb13b41e38fc66229a83af80d171cbd1b
Hecatomb <60758685+pedroelbanquero@users.noreply.github.com>
2021-09-13 18:52:12 -0500

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,6 +1,8 @@
 # compiler eos shell metamorph
 
-cython3 --embed -o shelleos.c shell-eos.py
+## python to c++
+cython3 --embed -o shelleos.c shelleos.py  -X language_level=3
+## compile c
 gcc -static shelleos.c -o shelleos -I /usr/include/python3.5 $(pkg-config --libs --cflags python3) -lpython2.7 -lm -lutil -ldl -lz -lexpat -lpthread -lc
 
 
